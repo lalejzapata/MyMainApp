a643bc61c71cbc5606dd1e37e8b6b45d2ea6b59e
Luis Zapata <lalejzapata@falabella.cl>
2024-09-27 13:18:10 -0300

    Initial Commit

---
+++ MyMainApp/MyMainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMainAppApp.swift
+//  MyMainApp
+//
+//  Created by Luis Alejandro Zapata Gonzalez on 27-09-24.
+//
+
+import SwiftUI
+
+@main
+struct MyMainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
